b6ce3b9509d28417ebc08c2fc29de197aa73efe2
Sithum Basnayaka <sithum.dashantha@gmail.com>
2023-06-17 22:40:24 +0530

    installed shadcn and created a login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,95 +1,141 @@
 import React from "react";
-import login_background from "../../images/login_background.jpg";
-import Paper from "@mui/material/Paper";
-import { Button, Grid, TextField, Typography } from "@mui/material";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
+import {Button, buttonVariants, Icons, Input, Label} from "../../components";
+import {cn} from "../../lib/utils";
+import {Link} from "react-router-dom";
+
+const Login = ({className, ...props}) => {
+
+    const [isLoading, setIsLoading] = React.useState(false)
+
+    async function onSubmit(event) {
+        event.preventDefault()
+        setIsLoading(true)
+
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 3000)
+    }
+
+    return (
 
 
-
-const Login = () => {
-
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-    
-  return (
-    <div
-      style={{ backgroundImage: `url(${login_background})`, height: "100vh" }}
-    >
-      <Grid
-        container
-        xs={12}
-        display={"flex"}
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-      >
-    <Paper elevation={3} style={{ width: "500px", padding: "20px" }}>
-        <Grid item display={"flex"} flexDirection={"column"} gap={2}>
-            <Typography color={"primary"}>
-              Enter Your Login Details
-            </Typography>
-            <TextField
-              fullWidth
-              variant="standard"
-              name="userName"
-              label="User Name"
+    <>
+        <div className="md:hidden">
+            <img
+                src="/assets/images/login_background.jpg"
+                width={1280}
+                height={843}
+                alt="Authentication"
+                className="block dark:hidden"
             />
-            <TextField
-              fullWidth
-              variant="standard"
-              name="password"
-              label="Password"
-            />
-        </Grid>
-
-        <Grid item xs={12} display={'flex'} justifyContent={'flex-start'} alignItems={'flex-start'}>
-            <Button color='error' style={{fontSize:'12px'}} onClick={handleClickOpen}>
-                Forget Password ?
-            </Button>
-      <     Dialog open={open} onClose={handleClose}>
-                <DialogContent>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-                </DialogContent>
-
-                <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleClose}>Subscribe</Button>
-                </DialogActions>
-            </Dialog>
-          </Grid>
-        <Grid
-            item
-            xs={12}
-            display={'flex'}
-            justifyContent={'flex-end'}
-            alignItems={'flex-end'}
-            marginTop={2}
-          >
-            <Button variant="outlined" color="primary">
-              LOGIN
-            </Button>
-          </Grid>
-    </Paper>
-      </Grid>
-    </div>
-  );
+        </div>
+        <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+            <Link
+                className={cn(
+                    buttonVariants({ variant: "ghost", size: "sm" }),
+                    "absolute right-4 top-4 md:right-8 md:top-8"
+                )}
+             to='/examples/authentication'>
+                Login
+            </Link>
+            <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+                <div
+                    className="absolute inset-0 bg-cover"
+                    style={{
+                        backgroundImage:
+                            "url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)",
+                    }}
+                />
+                <div className="relative z-20 flex items-center text-lg font-medium">
+                     Acme Inc
+                </div>
+                <div className="relative z-20 mt-auto">
+                    <blockquote className="space-y-2">
+                        <p className="text-lg">
+                            &ldquo;This library has saved me countless hours of work and
+                            helped me deliver stunning designs to my clients faster than
+                            ever before. Highly recommended!&rdquo;
+                        </p>
+                        <footer className="text-sm">Sofia Davis</footer>
+                    </blockquote>
+                </div>
+            </div>
+            <div className="lg:p-8">
+                <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                    <div className="flex flex-col space-y-2 text-center">
+                        <h1 className="text-2xl font-semibold tracking-tight">
+                            Create an account
+                        </h1>
+                        <p className="text-sm text-muted-foreground">
+                            Enter your email below to create your account
+                        </p>
+                    </div>
+                    <div className={cn("grid gap-6", className)} {...props}>
+                        <form onSubmit={onSubmit}>
+                            <div className="grid gap-2">
+                                <div className="grid gap-1">
+                                    <Label className="sr-only" htmlFor="email">
+                                        Email
+                                    </Label>
+                                    <Input
+                                        id="email"
+                                        placeholder="name@example.com"
+                                        type="email"
+                                        autoCapitalize="none"
+                                        autoComplete="email"
+                                        autoCorrect="off"
+                                        disabled={isLoading}
+                                    />
+                                </div>
+                                <Button disabled={isLoading}>
+                                    {isLoading && (
+                                        <Icons.spinner className="mr-2 h-4 w-4 animate-spin"/>
+                                    )}
+                                    Sign In with Email
+                                </Button>
+                            </div>
+                        </form>
+                        <div className="relative">
+                            <div className="absolute inset-0 flex items-center">
+                                <span className="w-full border-t"/>
+                            </div>
+                            <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+                            </div>
+                        </div>
+                        <Button variant="outline" type="button" disabled={isLoading}>
+                            {isLoading ? (
+                                <Icons.spinner className="mr-2 h-4 w-4 animate-spin"/>
+                            ) : (
+                                <Icons.gitHub className="mr-2 h-4 w-4"/>
+                            )}{" "}
+                            Github
+                        </Button>
+                    </div>
+                    <p className="px-8 text-center text-sm text-muted-foreground">
+                        By clicking continue, you agree to our{" "}
+                        <Link
+                            to="/terms"
+                            className="underline underline-offset-4 hover:text-primary"
+                        >
+                            Terms of Service
+                        </Link>{" "}
+                        and{" "}
+                        <Link
+                            to="/privacy"
+                            className="underline underline-offset-4 hover:text-primary"
+                        >
+                            Privacy Policy
+                        </Link>
+                        .
+                    </p>
+                </div>
+            </div>
+        </div>
+    </>
+    );
 };
 
 export default Login;
